4df9663c6700c31b41de54a9e984c46f33b124a4
sydneyani <135778566+sydneyani@users.noreply.github.com>
2023-11-10 14:50:57 -0600

    Update loginRoute.js

--- TodaysVision/Backend/routes/loginRoute.js
+++ TodaysVision/Backend/routes/loginRoute.js
@@ -6,4 +6,10 @@ const router = express.Router();
 // routes using prefix "/login"
 router.post('/',  (req, res) =>  loginController.login(req,res)); 
 
+// Handle GET requests to '/login'
+router.get('/', (req, res) => {
+    // You can add your logic here. For now, it will just send a response.
+    res.send('GET request to the login page');
+});
+
 module.exports = router;
